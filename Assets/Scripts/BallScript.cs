8cf10aceb2adb023ff8c72631078227815a4bd95
Daniel Velez <>
2022-01-05 13:22:24 -0500

    Initial Commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private Rigidbody ball;
+    public float force; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ball.AddForce(Vector3.forward * force);
+            
+        }
+
+    
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 colDirection = collision.contacts[0].point - transform.position;
+
+        colDirection = -colDirection.normalized;
+        if (!collision.gameObject.CompareTag("No score"))
+        {
+            ball.AddForce(colDirection * 1400);
+        }
+    }
+}
---
+++ Assets/Scripts/GameScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScore : MonoBehaviour
+{
+    private int score = 0;
+    // Start is called before the first frame update
+    void OnCollisonEnter(Collision Ball)
+    {
+
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!other.gameObject.CompareTag("No score"))
+            {
+            Debug.Log(score);
+            score = score + 1;
+            }
+           
+    }
+
+   
+}
